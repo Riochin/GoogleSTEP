9d720251250f0c3eef066d0bc4b84e8a1dc96164
りおちん <118029.ichikama@gmail.com>
2025-05-30 03:17:10 +0900

    clean: .txt -> text/

--- week1/hw2/Test.java
+++ week1/hw2/Test.java
@@ -8,13 +8,13 @@ import java.util.ArrayList;
 
 public class Test {
     public static void main(String[] args) throws FileNotFoundException {
-        System.setOut(new java.io.PrintStream("large_answer.txt")); // 出力をanswer.txtにリダイレクト
-        Scanner sc = new Scanner(new File("words.txt")); // ファイル名を書き換える
+        System.setOut(new java.io.PrintStream("text/small_answer.txt")); // 出力をanswer.txtにリダイレクト
+        Scanner sc = new Scanner(new File("text/words.txt")); // ファイル名を書き換える
         HashMap<String, ArrayList<String>> dict = Main.makeSortedDict(sc);
         sc.close();
         
         // 入力ファイル名をコマンドライン引数から取得（なければ input.txt）
-        String inputFile = args.length > 0 ? args[0] : "large.txt";
+        String inputFile = args.length > 0 ? args[0] : "text/small.txt";
         Scanner inputScanner = new Scanner(new File(inputFile));
         int testNum = 1;
         while (inputScanner.hasNextLine()) {
